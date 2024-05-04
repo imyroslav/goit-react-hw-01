76e9493e303abbd917f568b2678d58bd7d3f6c43
imyroslav <imyroslav@ukr.net>
2024-05-03 19:58:35 -0600

    created-frendlistitem

--- src/componets/frendlist/FrendList.jsx
+++ src/componets/frendlist/FrendList.jsx
@@ -1,12 +1,23 @@
 
 const FriendList = () => {
     return (
-    <ul>
-        <li>
-            <FriendListItem />
-        </li>
-    </ul>
-    )
+        <>
+            <ul>
+                <li>
+                    <FriendListItem />
+                </li>
+            </ul>
+        </>
+    
+    );
+};
+
+const FriendListItem = () => {
+    <div>
+        <img src="" alt="Avatar" width="48" />
+        <p>Friend name</p>
+        <p>Friend status</p>
+    </div>
 }
 
 export default FriendList;
